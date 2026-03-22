91921dacdf75c6c63a44e0b2c993bc5ed68e2ac4
su yeon kim <65233714+zion0117@users.noreply.github.com>
2026-03-22 18:13:20 +0900

    Auto-save: SpendingView.swift

--- TaskFlow/SpendingView.swift
+++ TaskFlow/SpendingView.swift
@@ -1022,7 +1022,8 @@ struct AddTransactionSheet: View {
 
     @State private var type = "expense"
     @State private var amountText = ""
-    @State private var category = "식비"
+    @State private var category = "중고거래"
+    @State private var subcategory = ""
     @State private var paymentMethod = "카드"
     @State private var store = ""
     @State private var customStores: [String] = UserDefaults.standard.stringArray(forKey: "customStores") ?? []
